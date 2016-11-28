3362b52558efdfc581b715471dc0da5b70e12d26
JD <623893416@qq.com>
2016-11-28 21:33:09 +0800

    在另外一个地方clone下来，提交SimpleView测试看下能否成功

---
+++ app/src/main/java/com/ugiant/myandroidview/view/SimpleView.java
@@ -0,0 +1,19 @@
+package com.ugiant.myandroidview.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by JDNew on 11/28/2016.
+ */
+
+public class SimpleView extends View {
+    public SimpleView(Context context) {
+        super(context);
+    }
+
+    public SimpleView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
