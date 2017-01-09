c94ed2b2547a923e833d29716dd7ea7599847a33
chijiaduo <623893416@qq.com>
2017-01-09 15:45:57 +0800

    修改部分文件

--- app/src/main/java/com/ugiant/myandroidview/MainActivity.java
+++ app/src/main/java/com/ugiant/myandroidview/MainActivity.java
@@ -4,14 +4,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.ugiant.myandroidview.activity.BottomWayActivity;
 
+
+
+import com.ugiant.myandroidview.activity.GetStartActivity;
+import com.ugiant.myandroidview.activity.PracticeActivity;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private TextView tv_bottom_way;
-    private LinearLayout activity_main;
+    private Button bt_get_start;
+    private Button bt_practice;
+
 
 //    private SimpleTopBar tb_title;
 
@@ -20,37 +28,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
-        setListener();
-//        tb_title = (SimpleTopBar) findViewById(R.id.tb_title);
-//        tb_title.setOnTopBarClickListener(new SimpleTopBar.topbarClickListener() {
-//            @Override
-//            public void leftClick() {
-//                Toast.makeText(MainActivity.this , "左边" , Toast.LENGTH_SHORT).show();
-//            }
-//
-//            @Override
-//            public void rightClick() {
-//                Toast.makeText(MainActivity.this , "右边" , Toast.LENGTH_SHORT).show();
-//            }
-//        });
+
+
 
 
     }
+
 
     private void setListener() {
-        tv_bottom_way.setOnClickListener(this);
+
     }
 
+
+
     private void initView() {
-        tv_bottom_way = (TextView) findViewById(R.id.tv_bottom_way);
-        activity_main = (LinearLayout) findViewById(R.id.activity_main);
+        bt_get_start = (Button) findViewById(R.id.bt_get_start);
+        bt_practice = (Button) findViewById(R.id.bt_practice);
+
+        bt_get_start.setOnClickListener(this);
+        bt_practice.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.tv_bottom_way:
-startActivity(new Intent(this , BottomWayActivity.class));
+
+        switch (v.getId()) {
+            case R.id.bt_get_start:
+         startActivity(new Intent(MainActivity.this , GetStartActivity.class));
+                break;
+            case R.id.bt_practice:
+                startActivity(new Intent(MainActivity.this , PracticeActivity.class));
+
                 break;
         }
     }
